5a3f84f937f787eb159b5c45c2c9cebe9070fb37
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2022-11-05 11:07:40 +1100

    Create basic states to hold app info

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { useState } from "react";
+import words from "./wordList.json";
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+ const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
   return <p>Hangman Initial Commit</p>;
 }
-
 export default App;
